c6c4f37f77586bec3a05961eb2e71e63978398f5
João Vítor <32188614+JoaovitorFerreira@users.noreply.github.com>
2020-06-06 16:38:08 -0300

    finalizacao do projeto

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,12 @@
-import express from "express";
+import express, { request, response } from "express";
+import routes from "./routes";
+import path from 'path';
+import cors from 'cors';
 
 const app = express();
 
-app.get("/users", (request, response) => {
-  console.log("Usuarios");
-
-  response.json(['Diego', 'Cleyton','Robson','Diogo','Arlindo']);
-});
-
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.use('/uploads',express.static(path.resolve(__dirname,'..','uploads')))
 app.listen(3333);
